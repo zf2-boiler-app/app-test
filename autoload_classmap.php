32c40c63f8dd1081012542100be1cc2a4a957fcf
neilime <escalle.emilien@gmail.com>
2013-04-01 01:02:41 +0200

    Update autoloading

--- autoload_classmap.php
+++ autoload_classmap.php
@@ -1,3 +1,6 @@
 <?php
 return array(
+	'BoilerAppPHPUnit\PHPUnit\Testcase\AbstractTestCase' => __DIR__.'/src/BoilerAppPHPUnit/PHPUnit/Testcase/AbstractTestCase.php',
+	'BoilerAppPHPUnit\PHPUnit\Testcase\DoctrineTestCase' => __DIR__.'/src/BoilerAppPHPUnit/PHPUnit/Testcase/DoctrineTestCase.php',
+	'BoilerAppPHPUnit\PHPUnit\Testcase\ModuleTestCase' => __DIR__.'/src/BoilerAppPHPUnit/PHPUnit/Testcase/ModuleTestCase.php'
 );
