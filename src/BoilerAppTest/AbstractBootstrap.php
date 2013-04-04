d8f4548b303574727aad7ae5798c755769c9ef33
neilime <escalle.emilien@gmail.com>
2013-04-04 13:54:29 +0200

    Improve Application init

--- src/BoilerAppTest/AbstractBootstrap.php
+++ src/BoilerAppTest/AbstractBootstrap.php
@@ -2,19 +2,21 @@
 namespace BoilerAppTest;
 abstract class AbstractBootstrap{
     /**
-     * @var \Zend\ServiceManager\ServiceManager
+     * @var \Zend\Mvc\Application
      */
-	protected static $serviceManager;
+    private static $application;
 
-	/**
-	 * @var array
-	 */
-	protected static $config;
+    /**
+     * @var array
+     */
+    private static $applicationConfig;
 
     /**
      * Initialize bootstrap
      */
     public static function init(){
+        static::initAutoloader();
+
         //Load the user-defined test configuration file, if it exists;
         if(is_readable($sConfigPath = getcwd() . '/TestConfig.php'))$aTestConfig = include $sConfigPath;
         elseif(is_readable($sConfigDistPath = getcwd() . '/TestConfig.php.dist'))$aTestConfig = include $sConfigDistPath;
@@ -24,63 +26,39 @@ abstract class AbstractBootstrap{
         if(isset($aTestConfig['module_listener_options']['module_paths']))foreach($aTestConfig['module_listener_options']['module_paths'] as $sModulePath){
         	if(($sPath = static::findParentPath($sModulePath)))$aZf2ModulePaths[] = $sPath;
         }
-        static::initAutoloader();
 
         //Use ModuleManager to load this module and it's dependencies
-        static::$config = \Zend\Stdlib\ArrayUtils::merge(array(
+        self::setApplicationConfig(\Zend\Stdlib\ArrayUtils::merge(array(
             'module_listener_options' => array(
                 'module_paths' => array_merge(
                 	$aZf2ModulePaths,
                 	explode(PATH_SEPARATOR, (getenv('ZF2_MODULES_TEST_PATHS')?:(defined('ZF2_MODULES_TEST_PATHS')?ZF2_MODULES_TEST_PATHS:'')))
                 )
             )
-        ),$aTestConfig);
-        static::$serviceManager = new \Zend\ServiceManager\ServiceManager(new \Zend\Mvc\Service\ServiceManagerConfig());
-        static::$serviceManager->setService('ApplicationConfig',static::$config)->get('ModuleManager')->loadModules();
-
-        if(is_readable($sConfigurationPath = getcwd().'/configuration.php')){
-        	$aConfiguration = \Zend\Stdlib\ArrayUtils::merge(static::$serviceManager->get('Config'),include $sConfigurationPath);
-        	$bAllowOverride = static::$serviceManager->getAllowOverride();
-        	if(!$bAllowOverride)static::$serviceManager->setAllowOverride(true);
-        	static::$serviceManager->setService('Config',$aConfiguration)->setAllowOverride($bAllowOverride);
-        }
-    }
-
-    /**
-     * @return \Zend\ServiceManager\ServiceManager
-     */
-    public static function getServiceManager(){
-        return static::$serviceManager;
-    }
-
-    /**
-     * @return array
-     */
-    public static function getConfig(){
-        return static::$config;
+        ),$aTestConfig));
     }
 
     /**
      * Initialize Autoloader
      * @throws RuntimeException
      */
-    protected static function initAutoloader(){
-        if(is_readable($sAutoloadPath = static::findParentPath('vendor').DIRECTORY_SEPARATOR.'autoload.php'))include $sAutoloadPath;
-        if(!class_exists('Zend\Loader\AutoloaderFactory'))throw new \RuntimeException('Unable to load ZF2. Install required libraries through `composer`');
+    private static function initAutoloader(){
+    	if(is_readable($sAutoloadPath = static::findParentPath('vendor').DIRECTORY_SEPARATOR.'autoload.php'))include $sAutoloadPath;
+    	if(!class_exists('Zend\Loader\AutoloaderFactory'))throw new \RuntimeException('Unable to load ZF2. Install required libraries through `composer`');
 
-        $oReflectionClass = new \ReflectionClass(get_called_class());
-        $sNamespace = $oReflectionClass->getNamespaceName();
+    	$oReflectionClass = new \ReflectionClass(get_called_class());
+    	$sNamespace = $oReflectionClass->getNamespaceName();
 
-        $aNamespaces = array();
-        if(is_dir($sFixturesDir = getcwd().DIRECTORY_SEPARATOR.'Fixture'))$aNamespaces[$sNamespace.'\Fixture'] = $sFixturesDir;
-        $aNamespaces[$sNamespace] = getcwd().DIRECTORY_SEPARATOR.$sNamespace;
+    	$aNamespaces = array();
+    	if(is_dir($sFixturesDir = getcwd().DIRECTORY_SEPARATOR.'Fixture'))$aNamespaces[$sNamespace.'\Fixture'] = $sFixturesDir;
+    	$aNamespaces[$sNamespace] = getcwd().DIRECTORY_SEPARATOR.$sNamespace;
 
-        \Zend\Loader\AutoloaderFactory::factory(array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'autoregister_zf' => true,
-                'namespaces' => $aNamespaces
-            )
-        ));
+    	\Zend\Loader\AutoloaderFactory::factory(array(
+    		'Zend\Loader\StandardAutoloader' => array(
+    			'autoregister_zf' => true,
+    			'namespaces' => $aNamespaces
+    		)
+    	));
     }
 
     /**
@@ -88,14 +66,53 @@ abstract class AbstractBootstrap{
      * @param string $sPath
      * @return boolean|string
      */
-    protected static function findParentPath($sPath){
-        $sCurrentDir = getcwd();
-        $sPreviousDir = '.';
-        while(!is_dir($sPreviousDir . '/' . $sPath)){
-            $sCurrentDir = dirname($sCurrentDir);
-            if($sPreviousDir === $sCurrentDir)return false;
-            $sPreviousDir = $sCurrentDir;
-        }
-        return $sCurrentDir . '/' . $sPath;
+    private static function findParentPath($sPath){
+    	$sCurrentDir = getcwd();
+    	$sPreviousDir = '.';
+    	while(!is_dir($sPreviousDir . '/' . $sPath)){
+    		$sCurrentDir = dirname($sCurrentDir);
+    		if($sPreviousDir === $sCurrentDir)return false;
+    		$sPreviousDir = $sCurrentDir;
+    	}
+    	return $sCurrentDir . '/' . $sPath;
+    }
+
+    /**
+     * Get the application config
+     * @return array
+     */
+    public static function getApplicationConfig(){
+    	if(!is_array(self::$applicationConfig))throw new \LogicException('Application config is undefined');
+    	return self::$applicationConfig;
+    }
+
+    /**
+     * Set the application config
+     * @param array $aApplicationConfig
+     * @throws \LogicException
+     */
+    public static function setApplicationConfig(array $aApplicationConfig){
+    	if(null !== self::$application && null !== self::$applicationConfig)throw new \LogicException('Application config can not be set, the application is already built');
+    	if(isset($applicationConfig['module_listener_options']['config_cache_enabled']))$aApplicationConfig['module_listener_options']['config_cache_enabled'] = false;
+    	self::$applicationConfig = $aApplicationConfig;
+    }
+
+    /**
+     * Get the application object
+     * @return \Zend\Mvc\Application
+     */
+    public static function getApplication(){
+    	if(self::$application instanceof \Zend\Mvc\ApplicationInterface)return self::$application;
+    	self::$application = \Zend\Mvc\Application::init(self::getApplicationConfig());
+    	$oEventManager = self::$application->getEventManager();
+    	$oEventManager->detach(self::$application->getServiceManager()->get('SendResponseListener'));
+    	return self::$application;
+    }
+
+    /**
+     * @return \Zend\ServiceManager\ServiceManager
+     */
+    public static function getServiceManager(){
+    	return self::getApplication()->getServiceManager();
     }
 }
--- src/BoilerAppTest/PHPUnit/Testcase/AbstractHttpControllerTestCase.php
+++ src/BoilerAppTest/PHPUnit/Testcase/AbstractHttpControllerTestCase.php
@@ -7,8 +7,8 @@ abstract class AbstractHttpControllerTestCase extends \Zend\Test\PHPUnit\Control
 	public function setUp(){
 		//Retrieve service manager from bootstrap
 		if(class_exists($sBootstrapClass = current(explode('\\', get_called_class())).'\Bootstrap')){
-			if(is_callable(array($sBootstrapClass,'getConfig')))$this->setApplicationConfig(call_user_func(array($sBootstrapClass,'getConfig')));
-			else throw new \BadMethodCallException('Method "getConfig" is not callable in "'.$sBootstrapClass.'" class');
+			if(is_callable(array($sBootstrapClass,'getApplicationConfig')))$this->setApplicationConfig(call_user_func(array($sBootstrapClass,'getApplicationConfig')));
+			else throw new \BadMethodCallException('Method "getApplicationConfig" is not callable in "'.$sBootstrapClass.'" class');
 		}
 		else throw new \LogicException('Bootstrap class "'.$sBootstrapClass.'" does not exist');
 		parent::setUp();
